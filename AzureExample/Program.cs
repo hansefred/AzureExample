d55b6f38dc9ad3ce4ce6709fc95429f656accad7
Mark Linder <mark.linder@endress.com>
2023-10-18 14:19:58 +0200

    MySQL DB Provider

--- AzureExample/Program.cs
+++ AzureExample/Program.cs
@@ -12,7 +12,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 var connectionString = Environment.GetEnvironmentVariable("MYSQLCONNSTR_localdb");
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlServer(connectionString));
+    options.UseMySQL(connectionString));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
     .AddEntityFrameworkStores<ApplicationDbContext>();
